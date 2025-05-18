ef7ec1d5eeec05006ded7263a814009d95385f63
Aliasleo <ducnm1508@gmail.com>
2025-05-18 17:58:26 +0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,3 +1,15 @@
-public class Admin {
+public class Admin extends User{
+    public Admin(String userid, String userName, String userPassword, String userRegisterTime, String userRole) {
+        super(userid, userName, userPassword, userRegisterTime, userRole);
+        
+    }
+    public Admin() {
+        super();
+        super.userRole = "admin";
+    }
+    @Override
+    public String toString() { 
+        return "{\"user_id\":\"" + userid + "\", \"user_name\":\"" + userName + "\", \"user_password\":\"" + userPassword + "\", \"user_register_time\":\"" + userRegisterTime + "\",\"user_role\":\"" + userRole + "\"}";
+    }
 
 }

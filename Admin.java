03ecc521673879f57124b18df7c5a26edd167430
Aliasleo <ducnm1508@gmail.com>
2025-05-19 00:04:45 +0700

    add set get admin and customer

--- Admin.java
+++ Admin.java
@@ -7,6 +7,31 @@ public class Admin extends User{
         super();
         super.userRole = "admin";
     }
+    public String getUserId() {
+        return userid;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String getUserPassword() {
+        return userPassword;
+    }
+    public String getUserRegisterTime() {
+        return userRegisterTime;
+    }
+    public String getUserRole() {
+        return userRole;
+    }
+    public void setUserId(String userid) {
+        this.userid = userid;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+   
     @Override
     public String toString() { 
         return "{\"user_id\":\"" + userid + "\", \"user_name\":\"" + userName + "\", \"user_password\":\"" + userPassword + "\", \"user_register_time\":\"" + userRegisterTime + "\",\"user_role\":\"" + userRole + "\"}";
--- Customer.java
+++ Customer.java
@@ -11,6 +11,42 @@ public class Customer extends User {
         this.userEmail = "";
         this.userMobile = "";
     }
+    public String getUserEmail() {
+        return userEmail;
+    }
+    public String getUserMobile() {
+        return userMobile;
+    }
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+    public void setUserMobile(String userMobile) {
+        this.userMobile = userMobile;
+    }
+    public String getUserId() {
+        return userid;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String getUserPassword() {
+        return userPassword;
+    }
+    public String getUserRegisterTime() {
+        return userRegisterTime;
+    }
+    public String getUserRole() {
+        return userRole;
+    }
+    public void setUserId(String userid) {
+        this.userid = userid;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
     @Override
     public String toString() { 
         return "{\"user_id\":\"" + userid + "\", \"user_name\":\"" + userName + "\", \"user_password\":\"" + userPassword + "\", \"user_register_time\":\"" + userRegisterTime + "\",\"user_role\":\"" + userRole + "\",\"user_email\":\"" + userEmail + "\",\"user_mobile\":\"" + userMobile + "\"}";

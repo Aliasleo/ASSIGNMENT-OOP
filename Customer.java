b3abca38015681d515784e2ca868a8f5071f3d1a
Aliasleo <ducnm1508@gmail.com>
2025-05-18 17:59:21 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,4 +1,18 @@
-
 public class Customer extends User {
-    super()
+    public String userEmail;
+    public String userMobile;
+    public Customer(String userid, String userName, String userPassword, String userRegisterTime, String userRole, String userEmail, String userMobile) {
+        super(userid, userName, userPassword, userRegisterTime, userRole);
+        this.userEmail = userEmail;
+        this.userMobile = userMobile;
+    }
+    public Customer() {
+        super();
+        this.userEmail = "";
+        this.userMobile = "";
+    }
+    @Override
+    public String toString() { 
+        return "{\"user_id\":\"" + userid + "\", \"user_name\":\"" + userName + "\", \"user_password\":\"" + userPassword + "\", \"user_register_time\":\"" + userRegisterTime + "\",\"user_role\":\"" + userRole + "\",\"user_email\":\"" + userEmail + "\",\"user_mobile\":\"" + userMobile + "\"}";
+    }
 }

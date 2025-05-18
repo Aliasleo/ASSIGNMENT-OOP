d8b546d59b73bec56ab15de67fedb439768404f0
Aliasleo <ducnm1508@gmail.com>
2025-05-18 23:57:42 +0700

    Update ProductOperation.java

--- ProductOperation.java
+++ ProductOperation.java
@@ -1,5 +1,11 @@
-public class ProductOperation {
+import java.util.List;
+import java.util.Locale;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ProductOperation extends Product {
     private static ProductOperation productOperation;
+    private List<Product> allProducts = new ArrayList<>();
 
     public static ProductOperation getInstance(){
         if (productOperation == null) {
@@ -12,7 +18,22 @@ public class ProductOperation {
     public boolean deleteProduct(String productId) {
         return false;
     }
-    public List<Product> getProductListBy Keyword(String keyword){
+    public List<Product> getProductListByKeyword(String keyword){
+        List<Product> matchingProducts = new ArrayList<>();
+        if (keyword == null || keyword.trim().isEmpty()) {
+            System.out.println("Keyword is null or empty.");
+            return Collections.emptyList();
+        }
+        String lowerCaseKeyword = keyword.trim().toLowerCase(Locale.ENGLISH); //convert keyword to lowercase
+        System.out.println("Searching for products containing keyword: '" + keyword + "'");
+        for (Product product : allProducts){
+            String lowerCaseProductName = product.getproName().toLowerCase(Locale.ENGLISH);
+            if (lowerCaseProductName.contains(lowerCaseKeyword)) {
+                matchingProducts.add(product);//If it contains, add the product to the list
+            }
+        }
+        System.out.println("Found " + matchingProducts.size() + "matching products.");
+        return matchingProducts;
         
     }
 }
